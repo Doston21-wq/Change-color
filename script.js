202cb1711dd1744c743909b0578875b3a8c9b9fa
Dostonjon21 <rajabovdoston500@gmail.com>
2025-05-25 18:19:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ button.addEventListener('click', function() {
     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = randomColor;
 });
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
+//this change color
